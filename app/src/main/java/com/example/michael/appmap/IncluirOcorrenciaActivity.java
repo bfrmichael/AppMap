3f32ba830cc7fe6dd4e251b89f2afded66de9ce3
bfrmichael <bfr.michael@gmail.com>
2016-06-03 01:50:34 -0300

    Mudanças no incluir ocorrência: agora passando referência de imagem para  banco de dados e a armazenando a imagem no servidor

--- app/src/main/java/com/example/michael/appmap/IncluirOcorrenciaActivity.java
+++ app/src/main/java/com/example/michael/appmap/IncluirOcorrenciaActivity.java
@@ -1,16 +1,22 @@
 package com.example.michael.appmap;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +25,11 @@ import java.util.Map;
  */
 public class IncluirOcorrenciaActivity extends Activity {
 
+    private ImageView imagem;
+    private Bitmap bitmap;
+    private Bitmap imagemEnviada;
+    private String encodeImageString;
+    private Uri fileNameImage;
     private EditText titulo;
     private EditText descricao;
     private TextView exibir;
@@ -35,16 +46,30 @@ public class IncluirOcorrenciaActivity extends Activity {
         categoria = (Spinner) findViewById(R.id.categoria_ocorrencia);
         categoria.setAdapter(adapter);
 
+        //captura imagem enviada
+        Bundle imagem_enviada = getIntent().getExtras();
+        imagemEnviada = (Bitmap) imagem_enviada.get("imagem");
+        fileNameImage = (Uri) imagem_enviada.get("file_name");
+
+        this.imagem = (ImageView) findViewById(R.id.imagem_ocorrencia);
+        this.imagem.setImageBitmap(imagemEnviada);
+
+        BitmapDrawable drawable = (BitmapDrawable) imagem.getDrawable();
+        bitmap = drawable.getBitmap();
+
         this.titulo = (EditText) findViewById(R.id.titulo_ocorrencia);
         this.descricao = (EditText) findViewById(R.id.descricao_ocorrencia);
         this.exibir = (TextView) findViewById(R.id.exibir_titulo);
+
+        //converte imagem para binário
+        encodeImageAsync();
     }
 
     public void salvarOcorrencia(View v) {
 
         Editable titulo  = this.titulo.getText();
         Editable descricao = this.descricao.getText();
-        this.exibir.setText(titulo);
+        this.exibir.setText(String.valueOf(fileNameImage));
 
         Double latitude = null;
         Double longitude = null;
@@ -73,10 +98,34 @@ public class IncluirOcorrenciaActivity extends Activity {
         ocorrencia.put("id_category", "1");
         ocorrencia.put("id_status", "1");
         ocorrencia.put("id_user", "1");
+        ocorrencia.put("image", encodeImageString);
+        ocorrencia.put("file_name", String.valueOf(fileNameImage));
 
         new InsertOccurrenceAsync().execute();
     }
 
+    //async para codificar imagem em formado base64
+    public void encodeImageAsync() {
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground (Void...params){
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 50, stream);
+                byte[] imageByte = stream.toByteArray();
+                encodeImageString = Base64.encodeToString(imageByte, 0);
+
+                return "";
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+            }
+        }.execute(null, null, null);
+    }
+
+    //async para enviar informações via protócolo http
     private class InsertOccurrenceAsync extends AsyncTask<String, Void, String> {
 
         @Override
@@ -98,6 +147,9 @@ public class IncluirOcorrenciaActivity extends Activity {
             }
 
         }
+
+        @Override
+        protected void onPostExecute(String result) {}
     }
 
 }
